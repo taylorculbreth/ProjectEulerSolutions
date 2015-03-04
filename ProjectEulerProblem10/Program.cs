a37e05a4ce9ddbb64b325294ec0ef217e52b56ad
Taylor Culbreth <taylorculbreth@taylorculbreth.com>
2015-03-03 21:43:42 -0600

    Committing project

---
+++ ProjectEulerProblem10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblem10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largestNum = 2000000;
+            double tot = 0;
+            int i = 1;
+            int b = 1;
+
+
+            for (i = 1; i < 2000000; i+=2)
+            {
+                if ((i % 2 != 0) && (i % 3 != 0))
+                {
+                    for (b = 1; b < 2000000; b++)
+                    {
+                        if ((i % b == 0))
+                        {
+                            if ((i == b) || (i / b == i))
+                            {
+                                tot += i;
+                                Console.WriteLine(tot);
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("final tot:" + tot);
+            Console.ReadKey();
+
+        }
+    }
+}
